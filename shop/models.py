aa56c71a0ca79e2131af7de4b1de4617c8ac0156
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2022-11-03 17:26:23 +0300

    Написание тестов для проверки корзины

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,18 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import User
 from django.db import models
 
 # таблица Продукты
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.utils import timezone
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='product_name')
-    code = models.CharField(max_length=255, verbose_name='product_code')  # по этому полю различаем продукт (код продукта)
+    code = models.CharField(max_length=255,
+                            verbose_name='product_code')  # по этому полю различаем продукт (код продукта)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     unit = models.CharField(max_length=255, blank=True, null=True)  # единица измерения
     image_url = models.URLField(blank=True, null=True)  # ссылка на изображение продукта
@@ -12,18 +20,22 @@ class Product(models.Model):
 
     class Meta:  # все элементы отображаются по дате создания
         ordering = ['pk']
+
     def __str__(self):
         return f'name: {self.name}, price: {self.price}'
 
+
 # таблица Платежи
 class Payment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # поле связано с табл. User, имя модели User, on_delete - удаление платежей
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)  # поле связано с табл. User, имя модели User, on_delete - удаление платежей
     amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)  # сумма платежа
     time = models.DateTimeField(auto_now_add=True)  # время создания платежа, заполняется автоматически
     comment = models.TextField(blank=True, null=True)  # комментарий к платежу
 
     class Meta:
         ordering = ['pk']
+
     def __str__(self):
         return f'name: {self.user} , price: {self.amount}'
 
@@ -40,26 +52,69 @@ class Order(models.Model):
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # items = models.ManyToManyField(OrderItem, related_name='orders')  # возможность установить связь с OrderItem
-    status = models.CharField(max_length=32, choices=STATUS_CHOICES, default=STATUS_CART)  # статус покупки, выбор из трех элементов
+    status = models.CharField(max_length=32, choices=STATUS_CHOICES,
+                              default=STATUS_CART)  # статус покупки, выбор из трех элементов
     amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)  # сумма заказа
     creation_time = models.DateTimeField(auto_now_add=True)  # время создания заказа, заполняется автоматически
-    payment = models.ForeignKey(Payment, on_delete=models.PROTECT, blank=True, null=True)  # поле, связанное с платежами, которое совершит изменение статуса оплаты
+    payment = models.ForeignKey(Payment, on_delete=models.PROTECT, blank=True,
+                                null=True)  # поле, связанное с платежами, которое совершит изменение статуса оплаты
     comment = models.TextField(blank=True, null=True)  # комментарий к заказу
 
     class Meta:
         ordering = ['pk']
+
     def __str__(self):
         return f'name: {self.user}, amount: {self.amount}, status: {self.status}'
+
+    @staticmethod
+    def get_cart(user: User):
+        cart = Order.objects.filter(user=user,
+                                    status=Order.STATUS_CART
+                                    ).first()
+        if cart and (timezone.now() - cart.creation_time).days > 7:
+            cart.delete()
+            cart = None
+
+        if not cart:
+            cart = Order.objects.create(user=user,
+                                        status=Order.STATUS_CART,
+                                        amount=0
+                                        )
+        return cart
+
+    def get_amount(self):
+        amount = Decimal(0)
+        for item in self.orderitem_set.all():
+            amount += item.amount
+        return amount
 
 # каждый элемент заказа
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)  # связь с заказом
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)  # связь с Продуктом, удалить продукт просто так нельзя
+    product = models.ForeignKey(Product,
+                                on_delete=models.PROTECT)  # связь с Продуктом, удалить продукт просто так нельзя
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     discount = models.DecimalField(max_digits=20, decimal_places=2, default=0)  # возможная скидка, по умолчанию 0
 
     class Meta:
         ordering = ['pk']
+
     def __str__(self):
         return f'product: {self.product}, price: {self.price}'
+
+    @property
+    def amount(self):
+        return self.quantity * (self.price - self.discount)
+
+@receiver(post_save, sender=OrderItem) # сигнал, соответствующий сохранению объекта в базе данных
+def recalculate_order_amount_after_save(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
+
+@receiver(post_delete, sender=OrderItem) # сигнал, соответствующий удалению объекта из базы данных
+def recalculate_order_amount_after_delete(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
--- shop/tests.py
+++ shop/tests.py
@@ -1,5 +1,10 @@
+import zoneinfo
+from decimal import Decimal
+
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
+
 from shop.models import Product, Payment, OrderItem, Order
 
 
@@ -11,6 +16,7 @@ class TestDataBase(TestCase):
     # извлекаем супер пользователя
     def setUp(self):
         self.user = User.objects.get(username='root')
+        self.p = Product.objects.all().first()
 
     # проверяем, существует ли пользователь
     def test_user_exists(self):
@@ -46,3 +52,44 @@ class TestDataBase(TestCase):
         =============================================================================
         Добавим @staticmethod Order.get_cart(user)
         '''
+
+        # Нет корзины
+        self.assertEqual(self.find_cart_number(), 0)
+        # 2. Корзина только что создана
+        Order.get_cart(self.user)
+        self.assertEqual(self.find_cart_number(), 1)
+        # 3. Проверить, что получена так корзина, которая создана, а не создалась новая
+        Order.get_cart(self.user)
+        self.assertEqual(self.find_cart_number(), 1)
+
+    def test_cart_older_7_day(self):
+        '''Если корзина старше 7 дней, она должна быть удалена
+        1. Получаем корзину и искусственно состариваем ее
+        '''
+        cart = Order.get_cart(self.user)
+        cart.creation_time = timezone.datetime(2000, 1, 1, tzinfo=zoneinfo.ZoneInfo('UTC'))
+        cart.save()
+        cart = Order.get_cart(self.user)
+        self.assertEqual((timezone.now() - cart.creation_time).days, 0)
+
+    def test_recalculate_order_amount_after_changing_ordertime(self):
+        '''Перерасчет суммы заказа после каждого изменения
+        1. Получаем сумму до какого-либо изменения
+        2. -------''-------- после добавления элемента
+        3. -------''-------- после удаления элемента
+        '''
+
+        # 1. Получаем сумму до какого-либо изменения
+        cart = Order.get_cart(self.user)
+        self.assertEqual(cart.amount, Decimal(0))
+
+        # 2. -------''-------- после добавления элемента
+        i = OrderItem.objects.create(order=cart, product=self.p, price=2, quantity=2)
+        i = OrderItem.objects.create(order=cart, product=self.p, price=2, quantity=3)
+        cart = Order.get_cart(self.user)
+        self.assertEqual(cart.amount, Decimal(10))
+
+        # 3. -------''-------- после удаления элемента
+        i.delete()
+        cart = Order.get_cart(self.user)
+        self.assertEqual(cart.amount, Decimal(4))
